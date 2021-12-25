fd0fd643202a2ec0bf177a2be3ce35a593a12e27
e.bublikova <e.bublikova@cinet.ru>
2021-12-26 01:05:48 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import './styles/index.scss';
-// import './component.js';
 import $ from 'jquery';
 import "slick-carousel";
 import "slick-carousel/slick/slick.css";
-// require("slick-carousel/slick/slick-theme.css");
 
 $(function () {
 	$('.speakers-slider').slick({
-		//   lazyLoad: progressive,
 		slidesToShow: 3,
 		lazyLoad: 'progressive',
 		infinite: false,
-		// slidesToScroll: 0.68,
 		prevArrow: `
 	  <button type="button" class="arrow custom-prev">
 	  <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -24,7 +20,15 @@ $(function () {
 		<path d="M9 1L15.2415 7.04944L9 13.0989" stroke="white" stroke-width="1.5"/>
 		<path d="M0 7.25H14.5" stroke="white" stroke-width="1.5" stroke-linejoin="round"/>
 	</svg></button>`,
-		responsive: [{
+		responsive: [
+			{
+				breakpoint: 880,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+			{
 				breakpoint: 1024,
 				settings: {
 					slidesToShow: 2,
@@ -32,9 +36,9 @@ $(function () {
 				}
 			},
 			{
-				breakpoint: 780,
+				breakpoint: 1370,
 				settings: {
-					slidesToShow: 1,
+					slidesToShow: 2,
 					slidesToScroll: 1,
 				}
 			},
@@ -44,21 +48,20 @@ $(function () {
 		e.preventDefault();
 		const name = $('#name').val();
 		const email = $('#email').val();
-		// const phone = $('#phone').val();
+		const phone = $('#phone').val();
+		const regexPhone = /(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?/;
+		const regExEmail = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+		const validEmail = regExEmail.test(email);
+		const validPhone = regexPhone.test(phone);
 
 		$(".error").remove();
 
 		if (name.length < 1) {
-			$('#name').after('<span class="error">This field is required</span>');
-		}
-		if (email.length < 1) {
-			$('#email').after('<span class="error">This field is required</span>');
-		} else {
-			var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/;
-			var validEmail = regEx.test(email);
-			if (!validEmail) {
-				$('#email').after('<span class="error">Enter a valid email</span>');
-			}
+			$('#name').after('<span class="error">Введите имя</span>');
+		} if (!validEmail) {
+			$('#email').after('<span class="error">Введите корректный e-mail</span>');
+		} if (!validPhone) {
+			$('#phone').after('<span class="error">Введите корректный телефон</span>');
 		}
 	});
 });
